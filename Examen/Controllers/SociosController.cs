5a58c2ae8cf03bfd2459005fe971d64f8d966a51
Francisco-Samueza <franchesamueza@gmail.com>
2021-07-26 21:29:10 -0500

    Agregar archivos de proyecto.

---
+++ Examen/Controllers/SociosController.cs
@@ -0,0 +1,111 @@
+﻿using Examen.Data;
+using Examen.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Examen.Controllers
+{
+    public class SociosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public SociosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Socios1Controller
+        public ActionResult Index()
+        {
+            List<Socio> socios = new List<Socio>();
+            socios = _context.Socio.ToList();
+            return View(socios);
+        }
+
+        // GET: Socios1Controller/Details/5
+        public ActionResult Details(string id)
+        {
+
+            Socio socio = _context.Socio.FirstOrDefault(x => x.Cedula == id);
+            return View();
+        }
+
+        // GET: Socios1Controller/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Socios1Controller/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Socio socio)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    _context.Add(socio);
+                    _context.SaveChanges();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(socio);
+            }
+        }
+
+        // GET: Socios1Controller/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Socio socio = _context.Socio.FirstOrDefault(x => x.Cedula == Convert.ToString(id));
+            return View();
+        }
+
+        // POST: Socios1Controller/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Socio socio)
+        {
+            if(id!=Convert.ToInt32(socio.Cedula))
+            {
+                return RedirectToAction("Index");
+            }
+            try
+            {
+                _context.Update(socio);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(socio);
+            }
+        }
+
+        // GET: Socios1Controller/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Socios1Controller/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Examen/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Examen.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examen.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Socio> Socio { get; set; }
+        public virtual DbSet<Cuenta> Cuentas { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Socio>(a =>
+            {
+                a.HasKey(c => c.Cedula);
+                a.Property(c => c.Nombre).IsRequired().HasMaxLength(100).IsUnicode(false);
+                a.Property(c => c.Apellido).IsRequired().HasMaxLength(100).IsUnicode(false);
+                a.Property(c => c.Direccion).IsRequired().HasMaxLength(250).IsUnicode(false);
+                a.Property(c => c.Estado).IsRequired();
+            }
+            );
+            base.OnModelCreating(builder);
+            builder.Entity<Cuenta>(a =>
+            {
+                a.HasKey(c => c.Numero);
+                a.Property(c => c.SaldoTotal).IsRequired().HasMaxLength(100).IsUnicode(false);
+                a.Property(c => c.CodigoSocio).IsRequired().HasMaxLength(100).IsUnicode(false);
+                a.Property(c => c.Estado).IsRequired();
+            }
+            );
+        }
+    }
+}
